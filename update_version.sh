abba09ff83ac719acaed0f6b697636d0919ebdf8
anatol <anatol@beam-mw.com>
2022-05-13 18:12:52 +0300

    updated version script

--- update_version.sh
+++ update_version.sh
@@ -6,9 +6,11 @@ MANIFEST="src/manifest.json"
 REVISION=$(git rev-list HEAD --count)
 
 if [[ $OUTPUT =~ $REGEX ]]; then
-	VERSION="${BASH_REMATCH[3]}.$REVISION"
+	VERSION="0.0.$REVISION"
+	BEAM_VERSION="${BASH_REMATCH[3]}"
 	echo -n $VERSION > version.gen
-	sed "s/%BEAM_VERSION%/$VERSION/" $MANIFEST.in > $MANIFEST.tmp;
+	sed "s/%VERSION%/$VERSION/" $MANIFEST.in > $MANIFEST.tmp;
+	sed "s/%BEAM_VERSION%/$BEAM_VERSION/" $MANIFEST.tmp > $MANIFEST.tmp2;
 fi
 
 SUFFIX=${BASH_REMATCH[1]}
@@ -19,6 +21,7 @@ if [[ $NETWORK != '' ]]; then
 	NETWORK=" ($NETWORK)"
 fi
 
-sed "s/%BEAM_NETWORK%/$NETWORK/" $MANIFEST.tmp > $MANIFEST;
+sed "s/%BEAM_NETWORK%/$NETWORK/" $MANIFEST.tmp2 > $MANIFEST;
 
+rm $MANIFEST.tmp2
 rm $MANIFEST.tmp
